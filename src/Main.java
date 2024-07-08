6790b1d1ff23c82409d447200fc2f10121062c17
bhakti <bhaktibhanage4@gmail.com>
2024-07-08 23:36:20 +0530

    convert string to array

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String str= "bhakti";
+        for (int i=0; i<str.length(); i++){
+            char c =str.charAt(i);
+            System.out.println(c);;
+        }
+
+    }
+}
